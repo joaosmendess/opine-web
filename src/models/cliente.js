4267e8657b8ed49eb99b8d89120ad06de75d7d8c
joaosmendess <joaosilva0721@gmail.com>
2023-12-12 23:05:55 -0300

    Criando model de Cliente

---
+++ src/models/cliente.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequilize');
+
+const db = require('../db/connection');
+
+const Cliente = db.define('cliente', {
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    numero: {
+        type: Sequelize.STRING
+    },
+    veiculo: {
+        type: Sequelize.STRING
+    }
+});
+
+module.exports = Cliente
