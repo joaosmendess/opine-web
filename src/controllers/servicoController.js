77fe4f9b918afc66274f468fd8cef7817ab69b48
joaosmendess <joaosilva0721@gmail.com>
2023-12-16 20:15:00 -0300

    Criando metodo de atualizar serviço PUT.

--- src/controllers/servicoController.js
+++ src/controllers/servicoController.js
@@ -23,6 +23,26 @@ const servicoController = {
         } catch (error) {
             res.status(500).send(error.message);
         }
+    },
+
+    atualizarServico : async (req, res) => {
+        const id = req.params.id;
+        const {tipo_servico} = req.body;
+
+      try {
+        const servico = await Servico.findByPk(id);
+        if (servico) {
+            servico.tipo_servico = tipo_servico;
+
+            await servico.save();
+            res.send("Serviço atualizado com sucesso");
+        }else{
+            res.status(404).send('Serviço não encontrado.');
+        }
+      } catch (error) {
+        res.status(500).send('Erro ao atualizar o serviço: ' + error.message);
+      }
+
     }
 
 
--- src/routes/servicoRoutes.js
+++ src/routes/servicoRoutes.js
@@ -5,6 +5,8 @@ const servicoController = require('../controllers/servicoController');
 
 router.get('/servicos', servicoController.listarServico);
 router.post('/servicos', servicoController.adicionarServico);
+router.put('/servicos/:id', servicoController.atualizarServico);
+
 
 
 module.exports = router;
