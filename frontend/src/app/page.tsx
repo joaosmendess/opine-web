5a641c9de6150a441dc208754a64219b5cac4b4c
joaosmendess <joaosilva0721@gmail.com>
2023-12-26 22:11:45 -0300

    Iniciando front-end com next

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+       <h1>HELLO WORLD!!!</h1>
+        
+      </div>
+    </main>
+  )
+}
