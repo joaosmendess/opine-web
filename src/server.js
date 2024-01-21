c039f2e07246b08763202815cb73933454da0f74
joaosmendess <joaosilva0721@gmail.com>
2024-01-21 20:57:46 -0300

    resolvendo bug de server

--- src/server.js
+++ src/server.js
@@ -1,23 +1,29 @@
-require("dotenv").config();
-require ('cors')
-
-const app = express();
+// Primeiro, importe os módulos necessários
 const express = require('express');
-app.use(cors())
+require("dotenv").config();
+const cors = require('cors');
+
+// Agora, inicialize o express
+const app = express();
+
+// Use os middlewares
+app.use(cors());
 app.use(express.json());
 
+// Defina a porta
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
 
+// Importe as rotas
 const clienteRoutes = require('./routes/clienteRoutes');
-const servicoRoutes= require('./routes/servicoRoutes');
+const servicoRoutes = require('./routes/servicoRoutes');
 const avaliacaoRoutes = require('./routes/avaliacaoRoutes');
 
-
+// Use as rotas
 app.use('/api', clienteRoutes);
 app.use('/api', servicoRoutes);
 app.use('/api', avaliacaoRoutes);
 
-
+// Inicie o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
