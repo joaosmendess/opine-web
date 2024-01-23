5c17c7b0e14183570600102ca3a881c9c8e0b63a
joaosmendess <joaosilva0721@gmail.com>
2024-01-23 19:50:48 -0300

    refatorando connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,28 +1,14 @@
 const Sequelize = require('sequelize');
 
-// Inicialize o Sequelize com sua string de conexão
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      require: true, 
-      rejectUnauthorized: true
+      require: true,
+      rejectUnauthorized: process.env.NODE_ENV === "production",
     }
-  }
+  },
+  logging: false, // Desative o log de SQL no console
 });
-
-// Função para testar a conexão com o banco de dados
-function testDatabaseConnection() {
-  sequelize.authenticate()
-    .then(() => {
-      console.log('Conexão com o banco de dados foi estabelecida com sucesso.');
-    })
-    .catch(err => {
-      console.error('Não foi possível conectar ao banco de dados:', err);
-    });
-}
-
-// Chame a função para testar a conexão
-testDatabaseConnection();
 
 module.exports = sequelize;
