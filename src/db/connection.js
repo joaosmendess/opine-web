0fb290549bc3945e7f2a84b4ae73bf5654a1d498
joaosmendess <joaosilva0721@gmail.com>
2024-01-23 20:21:41 -0300

    ajustando o sequileze e .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,20 +1,14 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: process.env.NODE_ENV === "production",
+      rejectUnauthorized:true,
     }
   },
   logging: false, // Desative o log de SQL no console
 });
 
 module.exports = sequelize;
-
